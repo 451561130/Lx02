0e83bf45ee5ba4ad40698dffac3436fd3b5aef01
451561130 <54524899+451561130@users.noreply.github.com>
2019-09-02 15:11:48 +0800

    Add files via upload

---
+++ Model.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Model
+{
+    public class UserInfo
+    {
+        pubilc string userName { get; set; }
+        pubilc string passWord { get; set; }
+        pubilc string qq { get; set; }
+        pubilc string email { get; set; }
+        pubilc string type { get; set; }
+        pubilc string userImg { get; set; }
+    }
+    public class UserInfoNo : UserInfo
+    {
+        public int num { get; set; }
+    }
+    public class Page
+    {
+        public int pageIndex { get; set; }
+        public int pageSize { get; set; }
+    }
+}
---
+++ UnitTest1.cs
@@ -0,0 +1,24 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Newtonsoft.Json;
+using Microsoft.AspNetCore.Mvc;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            public void TestMethod1()
+            {
+                Console.WriteLine(DAL.UserInfo.Instance.UserCheck("Admin"));
+            }
+        }
+        public void TestMethod2()
+        {
+
+        }
+    }
+}
