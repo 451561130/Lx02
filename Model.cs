672be405c02a522131d77c89d989b4ba0e2c4ce8
451561130 <54524899+451561130@users.noreply.github.com>
2019-09-08 10:17:07 +0800

    Add files via upload

--- Model.cs
+++ Model.cs
@@ -4,20 +4,20 @@ namespace Model
 {
     public class UserInfo
     {
-        pubilc string userName { get; set; }
-        pubilc string passWord { get; set; }
-        pubilc string qq { get; set; }
-        pubilc string email { get; set; }
-        pubilc string type { get; set; }
-        pubilc string userImg { get; set; }
+        public string userName { get; set; }
+        public string passWord { get; set; }
+        public string qq { get; set; }
+        public string email { get; set; }
+        public string type { get; set; }
+        public string userImg { get; set; }
     }
-    public class UserInfoNo : UserInfo
+    public class UserInfoNo:UserInfo
     {
         public int num { get; set; }
     }
     public class Page
     {
         public int pageIndex { get; set; }
-        public int pageSize { get; set; }
+        public int pagesize { get; set; }
     }
 }
